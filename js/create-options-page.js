4859e1765141c1c82ed8fddf3f63a4c3df7c8bc9
Ashot Davitavyan <ashot.davitavyan.techx@gmail.com>
2025-08-01 11:29:24 +0400

    almost done with create-options-page

--- js/create-options-page.js
+++ js/create-options-page.js
@@ -1,3 +1,37 @@
-document.getElementById("overlay").addEventListener("click", function (){
-	const uploadModal = document.getElementById();
+document.getElementById("upload-button").addEventListener("click", function () {
+	const uploadModal = document.getElementById("file-upload-modal");
+	const overlay = document.getElementById("overlay");
+	uploadModal.style.display = "flex";
+	overlay.style.display = "block";
+});
+
+document.getElementById("overlay").addEventListener("click", function () {
+	const uploadModal = document.getElementById("file-upload-modal");
+	uploadModal.style.display = "none";
+	this.style.display = "none";
+});
+
+document.getElementById("file-input").addEventListener("change", function (event) {
+	const uploadModal = document.getElementById("file-upload-modal");
+	const overlay = document.getElementById("overlay");
+	const uploadedFile = event.target.files[0];
+
+	if (!uploadedFile) return;
+
+	const reader = new FileReader();
+	reader.onload = function (e) {
+		const fileContent = e.target.result;
+		console.log(fileContent);
+		uploadModal.style.display = "none";
+		overlay.style.display = "none";
+		document.getElementById("file-input").value = "";
+		// window.location.href = ;
+	}
+
+	reader.onerror = function (e) {
+		console.error("Error reading file", e);
+	}
+
+	reader.readAsText(uploadedFile);
+
 });
